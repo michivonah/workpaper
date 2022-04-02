d5a6f4120edcda567e77a2b73eae3ca607e30d86
Michi <michivonah@gmail.com>
2022-04-02 15:52:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+function updateDate(){
+  const dateTime = new Date();
+  var day = dateTime.getDate();
+  var month = dateTime.getMonth() + 1;
+  var year = dateTime.getFullYear();
+  if(day.toString().length < 2) day = "0" + day;
+  if(month.toString().length < 2) month = "0" + month;
+  var date = day + "." + month + "." + year;
+  document.getElementById('dateTxt').textContent = date;
+}
+
+function update(){
+  document.title = document.getElementById('topic').textContent + " - Workpaper";
+}
+
+function saveData(){
+  var data = document.getElementById('userContent').innerHTML;
+  localStorage.setItem("data", data);
+  console.log("Succesfully saved your data.");
+}
+
+function loadData(){
+  var data = localStorage.getItem("data");
+  if(data !== null){
+    document.getElementById('userContent').innerHTML = data;
+    console.log("Succesfully restored your data.");
+  }
+}
+
+function deleteData(){
+  localStorage.removeItem("data");
+  if(confirm("Are you sure you want to delete anything you created? :-C") == true){
+    localStorage.removeItem("data");
+    alert("Your data is deleted, the site will reload.");
+    location.reload();
+  }
+}
+
+function openEditor(){
+  document.getElementById('workspace').style.display = "block";
+  document.getElementById('introduction').style.display = "none";
+}
+
+function addPage(){
+  var firstPage = document.getElementById('paper');
+  var newPage = firstPage.cloneNode(true);
+  var container = document.getElementById('userContent');
+  newPage.id = "";
+  container.appendChild(newPage);
+}
